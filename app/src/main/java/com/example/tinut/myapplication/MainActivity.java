d76d57c683239144856ce80465f6fe93a4bcdb78
Tinu Tomy <tinut@kochi.trekois.com>
2016-11-23 15:38:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tinut/myapplication/MainActivity.java
@@ -0,0 +1,159 @@
+package com.example.tinut.myapplication;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
+    Button sendBtn;
+    EditText txtphoneNo;
+    EditText txtMessage;
+    String phoneNo;
+    String message;
+    double latitude,longitude;
+
+    Button btnShowLocation;
+    private Button button;
+    private EditText etPhoneno;
+    String phnum;
+    // GPSTracker class
+    GPSTracker gps;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(MainActivity.this);
+
+                // check if GPS enabled
+                if (gps.canGetLocation()) {
+
+                     latitude = gps.getLatitude();
+                     longitude = gps.getLongitude();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                } else {
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+
+        button = (Button) findViewById(R.id.buttonCall);
+        etPhoneno = (EditText) findViewById(R.id.editText1);
+        // add button listener
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+             phnum = etPhoneno.getText().toString();
+                sendSMSMessage();
+                onCall();
+
+            }
+        });
+
+        sendBtn = (Button) findViewById(R.id.btnSendSMS);
+        txtphoneNo = (EditText) findViewById(R.id.editText);
+        txtMessage = (EditText) findViewById(R.id.editText2);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendSMSMessage();
+            }
+        });
+    }
+
+    protected void sendSMSMessage() {
+        phoneNo = txtphoneNo.getText().toString();
+        message = txtMessage.getText().toString();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+
+            case 123:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    onCall();
+                } else {
+                    Log.d("TAG", "Call Permission Not Granted");
+                }
+                break;
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage( phnum , null, String.valueOf(longitude), null, null);
+                    Toast.makeText(getApplicationContext(), "SMS sent.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+
+            default:
+                break;
+
+        }
+    }
+
+    private void onCall() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    Integer.parseInt("123"));
+        } else {
+            String phnum = etPhoneno.getText().toString();
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:" + phnum));
+            startActivity(callIntent);
+            // startActivity(new Intent(Intent.ACTION_CALL).setData(Uri.parse(phnum)));
+        }
+    }
+
+}
+
